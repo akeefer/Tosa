c46d203eedcea956521818ad9539d9bea3c89c2b
Alan Keefer <akeefer@gmail.com>
2011-01-03 12:20:05 -0800

    Add in basic createdb/deletedb shell scripts for roblog

---
+++ roblog/createdb.sh
@@ -0,0 +1,2 @@
+mysql --user=root --password=root -e 'CREATE DATABASE roblog'
+mysql --user=root --password=root roblog < src/db/roblog.ddl
---
+++ roblog/dropdb.sh
@@ -0,0 +1,1 @@
+mysql --user=root --password=root -e 'DROP DATABASE roblog'
