3019be4866566bb90c1f4aa3fce69ae2f4015ef9
Alan Keefer <akeefer@gmail.com>
2011-01-18 21:48:13 -0800

    Add in DBCP and use it for some very primitive connection pooling.  At the very least, the number of connections needs to be configurable.  We might also want to make the data source more pluggable.

--- tosa/src/tosa/DBConnection.java
+++ tosa/src/tosa/DBConnection.java
@@ -1,8 +1,15 @@
 package tosa;
 
 import gw.util.GosuExceptionUtil;
+import org.apache.commons.dbcp.ConnectionFactory;
+import org.apache.commons.dbcp.DriverManagerConnectionFactory;
+import org.apache.commons.dbcp.PoolableConnectionFactory;
+import org.apache.commons.dbcp.PoolingDataSource;
+import org.apache.commons.pool.ObjectPool;
+import org.apache.commons.pool.impl.GenericObjectPool;
 import tosa.loader.DBTypeLoader;
 
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.ResultSet;
@@ -24,26 +31,23 @@ public class DBConnection {
   private ThreadLocal<Connection> _transaction;
 
   private DBTypeLoader _typeLoader;
+  private DataSource _dataSource;
 
 
   public DBConnection(String connUrl, DBTypeLoader typeLoader) {
     _connectURL = connUrl;
     _transaction = new ThreadLocal<Connection>();
     _typeLoader = typeLoader;
+    _dataSource = setupDataSource(connUrl);
   }
 
   public Connection connect() throws SQLException {
     Connection trans = _transaction.get();
     if (trans == null) {
-      try {
-        Class driverClass = Class.forName(getDriverName(_connectURL), true, _typeLoader.getModule().getClassLoader());
-        Driver driver = (Driver) (driverClass.newInstance());
-        return driver.connect(_connectURL, null);
-      } catch (Exception e) {
-        throw GosuExceptionUtil.forceThrow(e);
-      }
+      return _dataSource.getConnection();
+    } else {
+      return trans;
     }
-    return trans;
   }
 
   private static String getDriverName(String url) {
@@ -59,5 +63,23 @@ public class DBConnection {
 
   public ThreadLocal<Connection> getTransaction() {
     return _transaction;
+  }
+
+  public DataSource setupDataSource(String connectURI) {
+    // Ensure the JDBC driver class is loaded
+    try {
+      Class.forName(getDriverName(_connectURL), true, _typeLoader.getModule().getClassLoader());
+    } catch (ClassNotFoundException e) {
+      throw GosuExceptionUtil.forceThrow(e);
+    }
+
+    GenericObjectPool connectionPool = new GenericObjectPool(null);
+    connectionPool.setMinIdle( 1 );
+    connectionPool.setMaxActive( 10 );
+
+    ConnectionFactory connectionFactory = new DriverManagerConnectionFactory(connectURI,null);
+    PoolableConnectionFactory poolableConnectionFactory = new PoolableConnectionFactory(connectionFactory,connectionPool,null,null,false,true);
+
+    return new PoolingDataSource(connectionPool);
   }
 }
