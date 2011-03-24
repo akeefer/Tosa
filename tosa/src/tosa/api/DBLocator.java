6dd68cd93c787510e58a26d37c07461db31d860b
Alan Keefer <akeefer@guidewire.com>
2011-03-24 14:10:25 -0700

    Add in a new DBManagementTool with basic functionality to create or drop MySQL databases.
    This also required fleshing out the DBLocator class so that it could be used to locate databases.

--- tosa/src/tosa/api/DBLocator.java
+++ tosa/src/tosa/api/DBLocator.java
@@ -1,5 +1,10 @@
 package tosa.api;
 
+import gw.lang.reflect.TypeSystem;
+import tosa.loader.DBTypeLoader;
+
+import java.util.Collection;
+
 /**
  * Created by IntelliJ IDEA.
  * User: alan
@@ -11,8 +16,20 @@ public class DBLocator {
   // TODO - AHK - Think about this class name
   // TODO - AHK - Think about this API
 
+  public static IDatabase getDatabase() {
+    DBTypeLoader dbTypeLoader = TypeSystem.getTypeLoader(DBTypeLoader.class);
+    Collection<? extends IDatabase> allDatabases = dbTypeLoader.getAllDatabases();
+    if (allDatabases.size() == 0) {
+      throw new IllegalArgumentException("No databases found");
+    } else if (allDatabases.size() > 2) {
+      throw new IllegalArgumentException("getDatabase() can only be called if there is a single IDatabase instance.  Found " + allDatabases.size());
+    } else {
+      return allDatabases.iterator().next();
+    }
+  }
+
   public static IDatabase getDatabase(String packageName) {
-    // TODO - AHK
-    return null;
+    DBTypeLoader dbTypeLoader = TypeSystem.getTypeLoader(DBTypeLoader.class);
+    return dbTypeLoader.getTypeDataForNamespace( "test.testdb" );
   }
 }
