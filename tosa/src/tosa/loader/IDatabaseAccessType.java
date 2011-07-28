76ffdc60d0b7c9e8510474f284465b911bc3617f
unknown <Alan@.(none)>
2011-07-27 21:11:29 -0700

    Intermediate commit for type info changes

---
+++ tosa/src/tosa/loader/IDatabaseAccessType.java
@@ -0,0 +1,11 @@
+package tosa.loader;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Alan
+ * Date: 7/27/11
+ * Time: 9:06 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface IDatabaseAccessType {
+}
